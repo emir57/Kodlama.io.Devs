dde0951acd2b309bb4483d96a6d5cec87619c648
emir57 <emir.gurbuz06@hotmail.com>
2022-09-02 16:23:19 +0300

    Create ProgrammingLanguageProfiles

---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Features/ProgrammingLanguages/Profiles/ProgrammingLanguageProfiles.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Core.Persistence.Paging;
+using Kodlama.io.devs.Application.Features.ProgrammingLanguages.Dtos;
+using Kodlama.io.devs.Domain.Entities;
+
+namespace Kodlama.io.devs.Application.Features.ProgrammingLanguages.Profiles
+{
+    public class ProgrammingLanguageProfiles : Profile
+    {
+        public ProgrammingLanguageProfiles()
+        {
+            CreateMap<ProgrammingLanguage, CreatedProgrammingLanguageDto>().ReverseMap();
+
+            CreateMap<ProgrammingLanguage, UpdatedProgrammingLanguageDto>().ReverseMap();
+
+            CreateMap<ProgrammingLanguage, DeletedProgrammingLanguageDto>().ReverseMap();
+
+            CreateMap<ProgrammingLanguage, GetByIdProgrammingLanguageDto>().ReverseMap();
+
+            CreateMap<ProgrammingLanguage, ProgrammingLanguageListDto>().ReverseMap();
+
+            CreateMap<IPaginate<ProgrammingLanguage>, ProgrammingLanguageListModel>().ReverseMap();
+        }
+    }
+}
