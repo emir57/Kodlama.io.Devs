4de38796ee7cb459174e8310edac9034db786d1e
emir57 <emir.gurbuz06@hotmail.com>
2022-09-02 16:19:21 +0300

    GetByIdProgrammingLanguage

---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Features/ProgrammingLanguages/Dtos/GetByIdProgrammingLanguageDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Kodlama.io.devs.Application.Features.ProgrammingLanguages.Dtos
+{
+    public class GetByIdProgrammingLanguageDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Features/ProgrammingLanguages/Queries/GetByIdProgrammingLanguage/GetByIdProgrammingLanguageQuery.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Kodlama.io.devs.Application.Features.ProgrammingLanguages.Dtos;
+using Kodlama.io.devs.Application.Services.Repositories;
+using Kodlama.io.devs.Domain.Entities;
+using MediatR;
+
+namespace Kodlama.io.devs.Application.Features.ProgrammingLanguages.Queries.GetByIdProgrammingLanguage
+{
+    public class GetByIdProgrammingLanguageQuery : IRequest<GetByIdProgrammingLanguageDto>
+    {
+        public int Id { get; set; }
+
+        public class GetByIdProgrammingLanguageQueryHandler : IRequestHandler<GetByIdProgrammingLanguageQuery, GetByIdProgrammingLanguageDto>
+        {
+            private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
+            private readonly IMapper _mapper;
+
+            public GetByIdProgrammingLanguageQueryHandler(IProgrammingLanguageRepository programmingLanguageRepository, IMapper mapper)
+            {
+                _programmingLanguageRepository = programmingLanguageRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<GetByIdProgrammingLanguageDto> Handle(GetByIdProgrammingLanguageQuery request, CancellationToken cancellationToken)
+            {
+                ProgrammingLanguage programmingLanguage = await _programmingLanguageRepository.GetAsync(p => p.Id == request.Id);
+                //todo: check null entity
+
+                GetByIdProgrammingLanguageDto getByIdProgrammingLanguageDto = _mapper.Map<GetByIdProgrammingLanguageDto>(programmingLanguage);
+                return getByIdProgrammingLanguageDto;
+            }
+        }
+    }
+}
