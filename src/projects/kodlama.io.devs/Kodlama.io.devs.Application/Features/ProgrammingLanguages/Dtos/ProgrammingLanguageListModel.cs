35b617c6db2fb716f160de7aee6e243c764558ab
emir57 <emir.gurbuz06@hotmail.com>
2022-09-02 16:15:15 +0300

    ListProgrammingLanguage

---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Features/ProgrammingLanguages/Dtos/ProgrammingLanguageListModel.cs
@@ -0,0 +1,9 @@
+﻿using Core.Persistence.Paging;
+
+namespace Kodlama.io.devs.Application.Features.ProgrammingLanguages.Dtos
+{
+    public class ProgrammingLanguageListModel : BasePageableModel
+    {
+        public IList<ProgrammingLanguageListDto> Items { get; set; }
+    }
+}
---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Features/ProgrammingLanguages/Queries/ListProgrammingLanguage/ListProgrammingLanguageQuery.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Core.Application.Requests;
+using Core.Persistence.Paging;
+using Kodlama.io.devs.Application.Features.ProgrammingLanguages.Dtos;
+using Kodlama.io.devs.Application.Services.Repositories;
+using Kodlama.io.devs.Domain.Entities;
+using MediatR;
+
+namespace Kodlama.io.devs.Application.Features.ProgrammingLanguages.Queries.ListProgrammingLanguage
+{
+    public class ListProgrammingLanguageQuery : IRequest<ProgrammingLanguageListModel>
+    {
+        public PageRequest PageRequest { get; set; }
+        public class ListProgrammingLanguageQueryHandler : IRequestHandler<ListProgrammingLanguageQuery, ProgrammingLanguageListModel>
+        {
+            private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
+            private readonly IMapper _mapper;
+
+            public ListProgrammingLanguageQueryHandler(IProgrammingLanguageRepository programmingLanguageRepository, IMapper mapper)
+            {
+                _programmingLanguageRepository = programmingLanguageRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<ProgrammingLanguageListModel> Handle(ListProgrammingLanguageQuery request, CancellationToken cancellationToken)
+            {
+                IPaginate<ProgrammingLanguage> paginate = await _programmingLanguageRepository.GetListAsync(
+                    index: request.PageRequest.Page,
+                    size: request.PageRequest.PageSize);
+
+                ProgrammingLanguageListModel programmingLanguageListModel = _mapper.Map<ProgrammingLanguageListModel>(paginate);
+                return programmingLanguageListModel;
+            }
+        }
+    }
+}
