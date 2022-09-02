e6267d674f8887e57d8153e88261c16fcd3f6ba4
emir57 <emir.gurbuz06@hotmail.com>
2022-09-02 16:33:08 +0300

    ProgrammingLanguageRules

---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Features/ProgrammingLanguages/Constants/ProgrammingLanguageMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace Kodlama.io.devs.Application.Features.ProgrammingLanguages.Constants
+{
+    public class ProgrammingLanguageMessages
+    {
+        public static readonly string ProgrammingLanguageCannotBeDuplicated = "Programming language cannot be duplicated";
+
+        public static readonly string ProgrammingLanguageNotExists = "Programming language not exists";
+    }
+}
---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Features/ProgrammingLanguages/Rules/ProgrammingLanguageRules.cs
@@ -0,0 +1,31 @@
+﻿using Core.CrossCuttingConcerns.Exceptions;
+using Kodlama.io.devs.Application.Features.ProgrammingLanguages.Constants;
+using Kodlama.io.devs.Application.Services.Repositories;
+using Kodlama.io.devs.Domain.Entities;
+
+namespace Kodlama.io.devs.Application.Features.ProgrammingLanguages.Rules
+{
+    public class ProgrammingLanguageRules
+    {
+        private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
+
+        public ProgrammingLanguageRules(IProgrammingLanguageRepository programmingLanguageRepository)
+        {
+            _programmingLanguageRepository = programmingLanguageRepository;
+        }
+
+        public async Task ProgrammingLanguageNameCannotBeDuplicatedWhenInsertedOrUpdated(string name)
+        {
+            ProgrammingLanguage? programmingLanguage = await _programmingLanguageRepository.GetAsync(p => p.Name.ToLower() == name.ToLower());
+            if (programmingLanguage == null)
+                throw new BusinessException(ProgrammingLanguageMessages.ProgrammingLanguageCannotBeDuplicated); ;
+        }
+
+        public async Task ProgrammingLanguageShouldExistsWhenRequested(int id)
+        {
+            ProgrammingLanguage? programmingLanguage = await _programmingLanguageRepository.GetAsync(p => p.Id == id);
+            if (programmingLanguage == null)
+                throw new BusinessException(ProgrammingLanguageMessages.ProgrammingLanguageNotExists);
+        }
+    }
+}
