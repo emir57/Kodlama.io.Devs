8bdd0ed262525b68d569c82770159cd16f0bf5e6
emir57 <emir.gurbuz06@hotmail.com>
2022-09-02 15:31:33 +0300

    ProgrammingLanguageRepository

---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Services/Repositories/IProgrammingLanguageRepository.cs
@@ -0,0 +1,9 @@
+﻿using Core.Persistence.Repositories;
+using Kodlama.io.devs.Domain.Entities;
+
+namespace Kodlama.io.devs.Application.Services.Repositories
+{
+    public interface IProgrammingLanguageRepository : IAsyncRepository<ProgrammingLanguage>, IRepository<ProgrammingLanguage>
+    {
+    }
+}
---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Persistence/Repositories/ProgrammingLanguageRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.Persistence.Repositories;
+using Kodlama.io.devs.Application.Services.Repositories;
+using Kodlama.io.devs.Domain.Entities;
+using Kodlama.io.devs.Persistence.Contexts;
+
+namespace Kodlama.io.devs.Persistence.Repositories
+{
+    public class ProgrammingLanguageRepository : EfRepositoryBase<ProgrammingLanguage, KodlamaDevDbContext>, IProgrammingLanguageRepository
+    {
+        public ProgrammingLanguageRepository(KodlamaDevDbContext context) : base(context)
+        {
+        }
+    }
+}
