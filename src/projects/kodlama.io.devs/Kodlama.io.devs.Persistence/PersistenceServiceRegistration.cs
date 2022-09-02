5c66e90fd410933d9db9d2dc68239fa587a20e90
emir57 <emir.gurbuz06@hotmail.com>
2022-09-02 15:39:01 +0300

    AddPersistenceServices

---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Kodlama.io.devs.Application.Services.Repositories;
+using Kodlama.io.devs.Persistence.Contexts;
+using Kodlama.io.devs.Persistence.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kodlama.io.devs.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddDbContext<KodlamaDevDbContext>();
+
+            services.AddScoped<IProgrammingLanguageRepository, ProgrammingLanguageRepository>();
+
+            return services;
+        }
+    }
+}
--- src/projects/kodlama.io.devs/Kodlama.io.devs.WebAPI/Program.cs
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.WebAPI/Program.cs
@@ -1,3 +1,5 @@
+using Kodlama.io.devs.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+#region Reistrations
+
+builder.Services.AddPersistenceServices();
+
+#endregion
+
 
 var app = builder.Build();
 
