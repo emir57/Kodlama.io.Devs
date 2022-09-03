f38fff7b0c0daf86444d7698b2351a093043ae22
emir57 <emir.gurbuz06@hotmail.com>
2022-09-03 11:51:17 +0300

    CreateProgrammingLanguageCommandValidator

---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Features/ProgrammingLanguages/Commands/CreateProgrammingLanguage/CreateProgrammingLanguageCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace Kodlama.io.devs.Application.Features.ProgrammingLanguages.Commands.CreateProgrammingLanguage
+{
+    public class CreateProgrammingLanguageCommandValidator : AbstractValidator<CreateProgrammingLanguageCommand>
+    {
+        public CreateProgrammingLanguageCommandValidator()
+        {
+            RuleFor(c => c.Name)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(c => c.Name)
+                .MaximumLength(50);
+
+            RuleFor(c => c.Name)
+                .MinimumLength(1);
+        }
+    }
+}
