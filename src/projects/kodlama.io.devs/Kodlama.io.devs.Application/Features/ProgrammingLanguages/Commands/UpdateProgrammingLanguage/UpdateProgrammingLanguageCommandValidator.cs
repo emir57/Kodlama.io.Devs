d5a69558b40b85c5aa861ce459eacb88e6ca3c3e
emir57 <emir.gurbuz06@hotmail.com>
2022-09-03 11:52:41 +0300

    UpdateProgrammingLanguageCommandValidator

---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Features/ProgrammingLanguages/Commands/UpdateProgrammingLanguage/UpdateProgrammingLanguageCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace Kodlama.io.devs.Application.Features.ProgrammingLanguages.Commands.UpdateProgrammingLanguage
+{
+    public class UpdateProgrammingLanguageCommandValidator : AbstractValidator<UpdateProgrammingLanguageCommand>
+    {
+        public UpdateProgrammingLanguageCommandValidator()
+        {
+            RuleFor(c => c.Id)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(c => c.Name)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(c => c.Name)
+                .MaximumLength(50);
+
+            RuleFor(c => c.Name)
+                .MinimumLength(1);
+        }
+    }
+}
