a5d1d60117f64a15640684084ced1065133ee4c9
emir57 <emir.gurbuz06@hotmail.com>
2022-09-04 15:40:28 +0300

    Configure Domain Layer

--- src/projects/kodlama.io.devs/Kodlama.io.devs.Domain/Entities/ProgrammingLanguage.cs
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Domain/Entities/ProgrammingLanguage.cs
@@ -1,22 +1,21 @@
 ﻿using Core.Persistence.Repositories;
 
-namespace Kodlama.io.devs.Domain.Entities
+namespace Kodlama.io.devs.Domain.Entities;
+
+public class ProgrammingLanguage : Entity
 {
-    public class ProgrammingLanguage : Entity
+    public ProgrammingLanguage()
     {
-        public ProgrammingLanguage()
-        {
-        }
-
-        public ProgrammingLanguage(int id) : base(id)
-        {
-        }
-
-        public ProgrammingLanguage(int id, string name) : base(id)
-        {
-            Name = name;
-        }
-
-        public string Name { get; set; }
     }
+
+    public ProgrammingLanguage(int id) : base(id)
+    {
+    }
+
+    public ProgrammingLanguage(int id, string name) : base(id)
+    {
+        Name = name;
+    }
+
+    public string Name { get; set; }
 }
