6a4a08427f7cb33a43e1641f9f51d8fe929acd7b
emir57 <emir.gurbuz06@hotmail.com>
2022-09-04 15:41:48 +0300

    Configure ContextExtensions

--- src/corePackages/Core.Persistence/Extensions/ContextExtensions.cs
+++ src/corePackages/Core.Persistence/Extensions/ContextExtensions.cs
@@ -1,14 +1,13 @@
 ﻿using Microsoft.Extensions.Configuration;
 
-namespace Core.Persistence.Extensions
-{
-    public static class ContextExtensions
-    {
-        public static string GetConnectionString(this IConfigurationBuilder configurationBuilder, string connectionStringName)
-        {
-            IConfigurationRoot configurationRoot = configurationBuilder.Build();
+namespace Core.Persistence.Extensions;
 
-            return configurationRoot.GetConnectionString(connectionStringName);
-        }
+public static class ContextExtensions
+{
+    public static string GetConnectionString(this IConfigurationBuilder configurationBuilder, string connectionStringName)
+    {
+        IConfigurationRoot configurationRoot = configurationBuilder.Build();
+
+        return configurationRoot.GetConnectionString(connectionStringName);
     }
 }
