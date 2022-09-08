0457e1809fabdc783d94853ebd855009c26fd9de
emir57 <emir.gurbuz06@hotmail.com>
2022-09-08 18:58:14 +0300

    Entity -> Add CreatedAt, UpdatedAt, DeletedAt Properties

--- src/corePackages/Core.Persistence/Repositories/Entity.cs
+++ src/corePackages/Core.Persistence/Repositories/Entity.cs
@@ -3,6 +3,9 @@
 public class Entity
 {
     public int Id { get; set; }
+    public DateTime? CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public DateTime? DeletedAt { get; set; }
 
     public Entity()
     {
@@ -11,5 +14,9 @@ public class Entity
     public Entity(int id) : this()
     {
         Id = id;
+    }
+    public Entity(int id, DateTime createdAt) : this(id)
+    {
+        CreatedAt = createdAt;
     }
 }
