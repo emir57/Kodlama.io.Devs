8054b263b2dbc58d0884067927e52d68b3b08f57
emir57 <emir.gurbuz06@hotmail.com>
2022-09-08 18:54:44 +0300

    Create ProgrammingLanguageTechnology Entity

---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Domain/Entities/ProgrammingLanguageTechnology.cs
@@ -0,0 +1,22 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Kodlama.io.devs.Domain.Entities;
+
+public class ProgrammingLanguageTechnology : Entity
+{
+    public string Name { get; set; }
+    public int ProgrammingLanguageId { get; set; }
+    public ProgrammingLanguage? ProgrammingLanguage { get; set; }
+
+    public ProgrammingLanguageTechnology()
+    {
+        ProgrammingLanguage = new ProgrammingLanguage();
+    }
+
+    public ProgrammingLanguageTechnology(int id, string name, int programmingLanguageId) : this()
+    {
+        Id = id;
+        Name = name;
+        ProgrammingLanguageId = programmingLanguageId;
+    }
+}
