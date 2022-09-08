98574ca8a2b280f3dc2fef94bf293612c411e508
emir57 <emir.gurbuz06@hotmail.com>
2022-09-08 19:09:44 +0300

    Context -> override SaveChanges and Update State

--- src/projects/kodlama.io.devs/Kodlama.io.devs.Persistence/Contexts/KodlamaDevDbContext.cs
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Persistence/Contexts/KodlamaDevDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using Core.Persistence.Extensions;
+using Core.Persistence.Repositories;
 using Kodlama.io.devs.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.Extensions.Configuration;
 using System.Reflection;
 
@@ -21,6 +23,21 @@ public class KodlamaDevDbContext : DbContext
             .AddJsonFile("appsettings.json");
 
         optionsBuilder.UseSqlServer(configurationBuilder.GetConnectionString("SqlServer"));
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        IEnumerable<EntityEntry<Entity>> entries = ChangeTracker.Entries<Entity>();
+        foreach (EntityEntry<Entity> entry in entries)
+        {
+            var _ = entry.State switch
+            {
+                EntityState.Added => entry.Entity.CreatedAt = DateTime.Now,
+                EntityState.Modified => entry.Entity.UpdatedAt = DateTime.Now,
+                _ => DateTime.Now
+            };
+        }
+        return base.SaveChangesAsync(cancellationToken);
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
