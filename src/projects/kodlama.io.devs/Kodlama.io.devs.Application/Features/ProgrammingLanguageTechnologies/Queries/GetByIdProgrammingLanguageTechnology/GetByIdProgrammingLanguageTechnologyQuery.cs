7fff864024e82a3b842d0cc0451a4fc4c810004a
emir57 <emir.gurbuz06@hotmail.com>
2022-09-09 14:00:01 +0300

    Create GetByIdProgrammingLanguageTechnologyQueryHandler

---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Features/ProgrammingLanguageTechnologies/Queries/GetByIdProgrammingLanguageTechnology/GetByIdProgrammingLanguageTechnologyQuery.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Kodlama.io.devs.Application.Features.ProgrammingLanguageTechnologies.Dtos;
+using Kodlama.io.devs.Application.Services.Repositories;
+using Kodlama.io.devs.Domain.Entities;
+using MediatR;
+
+namespace Kodlama.io.devs.Application.Features.ProgrammingLanguageTechnologies.Queries.GetByIdProgrammingLanguageTechnology;
+
+public sealed class GetByIdProgrammingLanguageTechnologyQuery : IRequest<GetByIdProgrammingLanguageTechnologyDto>
+{
+    public int Id { get; set; }
+
+    public sealed class GetByIdProgrammingLanguageTechnologyQueryHandler : IRequestHandler<GetByIdProgrammingLanguageTechnologyQuery, GetByIdProgrammingLanguageTechnologyDto>
+    {
+        private readonly IProgrammingLanguageTechnologyRepository _rogrammingLanguageTechnologyRepository;
+        private readonly IMapper _mapper;
+
+        public GetByIdProgrammingLanguageTechnologyQueryHandler(IProgrammingLanguageTechnologyRepository rogrammingLanguageTechnologyRepository, IMapper mapper)
+        {
+            _rogrammingLanguageTechnologyRepository = rogrammingLanguageTechnologyRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<GetByIdProgrammingLanguageTechnologyDto> Handle(GetByIdProgrammingLanguageTechnologyQuery request, CancellationToken cancellationToken)
+        {
+            //todo: check null
+            ProgrammingLanguageTechnology programmingLanguageTechnology = await _rogrammingLanguageTechnologyRepository.GetAsync(p => p.Id == request.Id);
+
+            GetByIdProgrammingLanguageTechnologyDto result = _mapper.Map<GetByIdProgrammingLanguageTechnologyDto>(programmingLanguageTechnology);
+            return result;
+        }
+    }
+}
