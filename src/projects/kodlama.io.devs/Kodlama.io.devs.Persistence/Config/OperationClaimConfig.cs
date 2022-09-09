40c0023e755334fe954dc7fdd504ec28d3c2672e
emir57 <emir.gurbuz06@hotmail.com>
2022-09-09 16:18:59 +0300

    Create OperationClaimConfig

---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Persistence/Config/OperationClaimConfig.cs
@@ -0,0 +1,22 @@
+﻿using Core.Security.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Kodlama.io.devs.Persistence.Config;
+
+public class OperationClaimConfig : IEntityTypeConfiguration<OperationClaim>
+{
+    public void Configure(EntityTypeBuilder<OperationClaim> builder)
+    {
+        builder.ToTable("OperationClaims")
+            .HasKey(o => o.Id);
+
+        builder.Property(o => o.Id)
+            .HasColumnName("Id");
+
+        builder.Property(o => o.Name)
+            .HasColumnName("Name")
+            .IsRequired()
+            .HasMaxLength(50);
+    }
+}
