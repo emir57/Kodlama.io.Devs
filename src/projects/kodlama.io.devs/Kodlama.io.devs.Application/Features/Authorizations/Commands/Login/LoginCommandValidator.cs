60ba156ca10efb935b779b266519aeb2227dff1f
emir57 <emir.gurbuz06@hotmail.com>
2022-09-09 23:27:43 +0300

    Create LoginCommandValidator

---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Features/Authorizations/Commands/Login/LoginCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+
+namespace Kodlama.io.devs.Application.Features.Authorizations.Commands.Login;
+
+public sealed class LoginCommandValidator : AbstractValidator<LoginCommand>
+{
+    public LoginCommandValidator()
+    {
+        RuleFor(l => l.UserForLoginDto.Email)
+            .NotEmpty()
+            .NotNull();
+        RuleFor(l => l.UserForLoginDto.Email)
+            .EmailAddress();
+        RuleFor(l => l.UserForLoginDto.Email)
+            .MaximumLength(50);
+
+        RuleFor(l => l.UserForLoginDto.Password)
+            .NotEmpty()
+            .NotNull();
+        RuleFor(l => l.UserForLoginDto.Password)
+            .MinimumLength(6);
+        RuleFor(l => l.UserForLoginDto.Password)
+            .MaximumLength(25);
+    }
+}
