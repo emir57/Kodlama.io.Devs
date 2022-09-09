c8bfd0dbe388eff26ab192b95f67c5907af55c58
emir57 <emir.gurbuz06@hotmail.com>
2022-09-09 13:45:50 +0300

    Create ProgrammingLanguageTechnologyProfiles

---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Features/ProgrammingLanguageTechnologies/Profiles/ProgrammingLanguageTechnologyProfiles.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Core.Persistence.Paging;
+using Kodlama.io.devs.Application.Features.ProgrammingLanguageTechnologies.Dtos;
+using Kodlama.io.devs.Application.Features.ProgrammingLanguageTechnologies.Models;
+using Kodlama.io.devs.Domain.Entities;
+
+namespace Kodlama.io.devs.Application.Features.ProgrammingLanguageTechnologies.Profiles;
+
+public sealed class ProgrammingLanguageTechnologyProfiles : Profile
+{
+    public ProgrammingLanguageTechnologyProfiles()
+    {
+        CreateMap<ProgrammingLanguageTechnology, CreateProgrammingLanguageTechnologyDto>().ReverseMap();
+        CreateMap<ProgrammingLanguageTechnology, UpdateProgrammingLanguageTechnologyDto>().ReverseMap();
+
+        CreateMap<IPaginate<ProgrammingLanguageTechnology>, ProgrammingLanguageTechnologyListModel>().ReverseMap();
+
+        CreateMap<ProgrammingLanguageTechnology, ProgrammingLanguageTechnologyListDto>()
+            .ForMember(x => x.ProgrammingLanguageName, opt => opt.MapFrom(x => x.ProgrammingLanguage.Name))
+            .ReverseMap();
+
+        CreateMap<ProgrammingLanguageTechnology, GetByIdProgrammingLanguageTechnologyDto>()
+            .ForMember(x => x.ProgrammingLanguageName, opt => opt.MapFrom(x => x.ProgrammingLanguage.Name))
+            .ReverseMap();
+    }
+}
