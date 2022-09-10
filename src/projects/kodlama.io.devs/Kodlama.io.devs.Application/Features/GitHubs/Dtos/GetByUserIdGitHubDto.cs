cb9670a82b7eacfea66b12046e04f4ea7c7942b7
emir57 <emir.gurbuz06@hotmail.com>
2022-09-10 21:14:35 +0300

    Configure GitHubProfiles

---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Features/GitHubs/Dtos/GetByUserIdGitHubDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Kodlama.io.devs.Application.Features.GitHubs.Dtos;
+
+public sealed class GetByUserIdGitHubDto
+{
+    public int Id { get; set; }
+    public string ProfileUserName { get; set; }
+    public string UserFullName { get; set; }
+    public string UserEmail { get; set; }
+}
--- src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Features/GitHubs/Profiles/GitHubProfiles.cs
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Features/GitHubs/Profiles/GitHubProfiles.cs
@@ -12,17 +12,23 @@ public sealed class GitHubProfiles : Profile
     public GitHubProfiles()
     {
         CreateMap<GitHub, CreateGitHubCommand>().ReverseMap();
-        CreateMap<GitHub, UpdateGitHubCommand>();
         CreateMap<UpdateGitHubCommand, GitHub>()
             .ForMember(x => x.UserId, opt => opt.Ignore())
             .ForMember(x => x.CreatedAt, opt => opt.Ignore())
             .ForMember(x => x.UpdatedAt, opt => opt.Ignore())
-            .ForMember(x => x.DeletedAt, opt => opt.Ignore());
+            .ForMember(x => x.DeletedAt, opt => opt.Ignore())
+            .ReverseMap();
         CreateMap<GitHub, DeleteGitHubCommand>();
 
 
         CreateMap<GitHub, UpdatedGitHubDto>().ReverseMap();
         CreateMap<GitHub, CreatedGitHubDto>().ReverseMap();
         CreateMap<GitHub, DeletedGitHubDto>().ReverseMap();
+
+        CreateMap<GitHub, GetByUserIdGitHubDto>()
+            .ForMember(x => x.UserFullName, opt => opt.MapFrom(g => $"{g.User.FirstName} {g.User.LastName}"))
+            .ForMember(x => x.UserEmail, opt => opt.MapFrom(g => g.User.Email))
+            .ReverseMap();
+
     }
 }
