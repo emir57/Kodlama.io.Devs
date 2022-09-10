b082ff9677811ff4161b768135e621375773cba8
emir57 <emir.gurbuz06@hotmail.com>
2022-09-10 20:48:45 +0300

    Create UpdateGitHubCommandValidator

---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Features/GitHubs/Commands/UpdateGitHub/UpdateGitHubCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+
+namespace Kodlama.io.devs.Application.Features.GitHubs.Commands.UpdateGitHub;
+
+public sealed class UpdateGitHubCommandValidator : AbstractValidator<UpdateGitHubCommand>
+{
+    public UpdateGitHubCommandValidator()
+    {
+        RuleFor(u => u.Id)
+            .NotEmpty()
+            .NotNull();
+        RuleFor(u => u.Id)
+            .GreaterThan(0);
+
+        RuleFor(c => c.ProfileUserName)
+            .NotEmpty()
+            .NotNull();
+
+        RuleFor(c => c.ProfileUserName)
+            .MinimumLength(1);
+
+        RuleFor(c => c.ProfileUserName)
+            .MaximumLength(50);
+    }
+}
---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Features/GitHubs/Dtos/DeletedGitHubDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Kodlama.io.devs.Application.Features.GitHubs.Dtos;
+
+public sealed class DeletedGitHubDto
+{
+    public int Id { get; set; }
+    public string ProfileUserName { get; set; }
+}
