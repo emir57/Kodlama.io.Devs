095ec70619fba8b3b607557daf1209f52cf86e75
emir57 <emir.gurbuz06@hotmail.com>
2022-09-10 14:19:50 +0300

    Create GitHub Entity

---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Domain/Entities/GitHub.cs
@@ -0,0 +1,26 @@
+﻿using Core.Persistence.Repositories;
+using Core.Security.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Kodlama.io.devs.Domain.Entities;
+
+public sealed class GitHub : Entity
+{
+    public string ProfileUserName { get; set; }
+    public int UserId { get; set; }
+    public User User { get; set; }
+
+    public string ProfileUrl
+        => string.Format("https://github.com/{0}", ProfileUserName);
+
+    public GitHub()
+    {
+        new User();
+    }
+    public GitHub(int id, string profileUserName, int userId) : this()
+    {
+        Id = id;
+        ProfileUserName = profileUserName;
+        UserId = userId;
+    }
+}
