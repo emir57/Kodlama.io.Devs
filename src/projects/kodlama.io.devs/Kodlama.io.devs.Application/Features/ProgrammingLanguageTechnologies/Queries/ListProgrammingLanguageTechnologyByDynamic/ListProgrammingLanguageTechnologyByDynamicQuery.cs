662a75074ed6c759cf1e29f71f9effe7910ba8c8
emir57 <emir.gurbuz06@hotmail.com>
2022-09-11 14:22:21 +0300

    Complete ListProgrammingLanguageTechnologyByDynamicQuery

---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Features/ProgrammingLanguageTechnologies/Queries/ListProgrammingLanguageTechnologyByDynamic/ListProgrammingLanguageTechnologyByDynamicQuery.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Core.Application.Requests;
+using Core.Persistence.Dynamic;
+using Core.Persistence.Paging;
+using Kodlama.io.devs.Application.Features.ProgrammingLanguageTechnologies.Models;
+using Kodlama.io.devs.Application.Services.Repositories;
+using Kodlama.io.devs.Domain.Entities;
+using MediatR;
+
+namespace Kodlama.io.devs.Application.Features.ProgrammingLanguageTechnologies.Queries.ListProgrammingLanguageTechnologyByDynamic;
+
+public sealed class ListProgrammingLanguageTechnologyByDynamicQuery : IRequest<ProgrammingLanguageTechnologyListModel>
+{
+    public Dynamic Dynamic { get; set; }
+    public PageRequest PageRequest { get; set; }
+
+    public sealed class ListProgrammingLanguageTechnologyByDynamicQueryHandler : IRequestHandler<ListProgrammingLanguageTechnologyByDynamicQuery, ProgrammingLanguageTechnologyListModel>
+    {
+        private readonly IProgrammingLanguageTechnologyRepository _programmingLanguageTechnologyRepository;
+        private readonly IMapper _mapper;
+
+        public ListProgrammingLanguageTechnologyByDynamicQueryHandler(IProgrammingLanguageTechnologyRepository programmingLanguageTechnologyRepository, IMapper mapper)
+        {
+            _programmingLanguageTechnologyRepository = programmingLanguageTechnologyRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<ProgrammingLanguageTechnologyListModel> Handle(ListProgrammingLanguageTechnologyByDynamicQuery request, CancellationToken cancellationToken)
+        {
+            IPaginate<ProgrammingLanguageTechnology> list = await _programmingLanguageTechnologyRepository
+                .GetListByDynamicAsync(request.Dynamic,
+                index: request.PageRequest.Page, size: request.PageRequest.PageSize,
+                enableTracking: false);
+
+            ProgrammingLanguageTechnologyListModel result = _mapper.Map<ProgrammingLanguageTechnologyListModel>(list);
+            return result;
+        }
+    }
+}
--- src/projects/kodlama.io.devs/Kodlama.io.devs.WebAPI/Controllers/ProgrammingLanguageTechnologiesController.cs
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.WebAPI/Controllers/ProgrammingLanguageTechnologiesController.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Application.Requests;
+using Core.Persistence.Dynamic;
 using Kodlama.io.devs.Application.Features.ProgrammingLanguageTechnologies.Commands.CreateProgrammingLanguageTechnology;
 using Kodlama.io.devs.Application.Features.ProgrammingLanguageTechnologies.Commands.DeleteProgrammingLangaugeTechnology;
 using Kodlama.io.devs.Application.Features.ProgrammingLanguageTechnologies.Commands.UpdateProgrammingLangaugeTechnology;
@@ -6,6 +7,7 @@ using Kodlama.io.devs.Application.Features.ProgrammingLanguageTechnologies.Dtos;
 using Kodlama.io.devs.Application.Features.ProgrammingLanguageTechnologies.Models;
 using Kodlama.io.devs.Application.Features.ProgrammingLanguageTechnologies.Queries.GetByIdProgrammingLanguageTechnology;
 using Kodlama.io.devs.Application.Features.ProgrammingLanguageTechnologies.Queries.ListProgrammingLanguageTechnology;
+using Kodlama.io.devs.Application.Features.ProgrammingLanguageTechnologies.Queries.ListProgrammingLanguageTechnologyByDynamic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Kodlama.io.devs.WebAPI.Controllers;
@@ -26,6 +28,19 @@ public class ProgrammingLanguageTechnologiesController : BaseController
     public async Task<IActionResult> Get([FromRoute] GetByIdProgrammingLanguageTechnologyQuery request)
     {
         GetByIdProgrammingLanguageTechnologyDto result = await Mediator.Send(request);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    [Route("dynamic")]
+    public async Task<IActionResult> Dynamic([FromBody] Dynamic dynamic, PageRequest pageRequest)
+    {
+        ListProgrammingLanguageTechnologyByDynamicQuery request = new()
+        {
+            Dynamic = dynamic,
+            PageRequest = pageRequest
+        };
+        ProgrammingLanguageTechnologyListModel result = await Mediator.Send(request);
         return Ok(result);
     }
 
