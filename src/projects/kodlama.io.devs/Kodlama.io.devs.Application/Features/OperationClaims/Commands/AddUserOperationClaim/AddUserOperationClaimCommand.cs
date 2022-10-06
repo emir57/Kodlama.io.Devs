a3ca991f4b7d20432ae1aa41bf0a116bb44918c6
emir57 <emir.gurbuz06@hotmail.com>
2022-10-06 20:47:27 +0300

    Create AddedUserOperationClaimDto And AddUserOperationClaimCommand

---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Features/OperationClaims/Commands/AddUserOperationClaim/AddUserOperationClaimCommand.cs
@@ -0,0 +1,10 @@
+﻿using Kodlama.io.devs.Application.Features.OperationClaims.Dtos;
+using MediatR;
+
+namespace Kodlama.io.devs.Application.Features.OperationClaims.Commands.AddUserOperationClaim;
+
+public sealed class AddUserOperationClaimCommand : IRequest<AddedUserOperationClaimDto>
+{
+    public int UserId { get; set; }
+    public int OperationClaimId { get; set; }
+}
---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Features/OperationClaims/Dtos/AddedUserOperationClaimDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Kodlama.io.devs.Application.Features.OperationClaims.Dtos;
+
+public sealed class AddedUserOperationClaimDto
+{
+    public string OperationClaimName { get; set; }
+    public string UserEmail { get; set; }
+}
