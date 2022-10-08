9c0f5f67e8060a04f8fcd6638994a5e247173886
emir57 <emir.gurbuz06@hotmail.com>
2022-10-08 23:52:02 +0300

    UpdateOperationClaimCommandValidator

---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Features/OperationClaims/Commands/UpdateOperationClaim/UpdateOperationClaimCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace Kodlama.io.devs.Application.Features.OperationClaims.Commands.UpdateOperationClaim;
+
+public sealed class UpdateOperationClaimCommandValidator : AbstractValidator<UpdateOperationClaimCommand>
+{
+    public UpdateOperationClaimCommandValidator()
+    {
+        RuleFor(u => u.Id)
+            .NotEmpty()
+            .NotNull();
+
+        RuleFor(u => u.Name)
+            .NotEmpty()
+            .NotNull();
+
+        RuleFor(u => u.Name)
+            .MaximumLength(50);
+    }
+}
