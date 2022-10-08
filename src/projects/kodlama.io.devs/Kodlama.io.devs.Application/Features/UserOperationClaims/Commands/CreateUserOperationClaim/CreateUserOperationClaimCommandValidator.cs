b00dc88c350fbd8c7fac6625649281e28a3d59b2
emir57 <emir.gurbuz06@hotmail.com>
2022-10-09 00:22:17 +0300

    CreateUserOperationClaimCommandValidator

---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Features/UserOperationClaims/Commands/CreateUserOperationClaim/CreateUserOperationClaimCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Kodlama.io.devs.Application.Features.UserOperationClaims.Commands.CreateUserOperationClaim;
+
+public sealed class CreateUserOperationClaimCommandValidator : AbstractValidator<CreateUserOperationClaimCommand>
+{
+    public CreateUserOperationClaimCommandValidator()
+    {
+        RuleFor(c => c.UserId)
+            .NotEmpty()
+            .NotNull();
+
+        RuleFor(c => c.OperationClaimId)
+            .NotEmpty()
+            .NotNull();
+    }
+}
