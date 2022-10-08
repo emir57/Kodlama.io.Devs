153b6696fd31d41f79af72df25ba2ba84363ddf5
emir57 <emir.gurbuz06@hotmail.com>
2022-10-09 00:25:52 +0300

    DeleteUserOperationClaimCommandValidator

---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Features/UserOperationClaims/Commands/DeleteUserOperationClaimCommand/DeleteUserOperationClaimCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Kodlama.io.devs.Application.Features.UserOperationClaims.Commands.DeleteUserOperationClaimCommand;
+
+public sealed class DeleteUserOperationClaimCommandValidator : AbstractValidator<DeleteUserOperationClaimCommand>
+{
+    public DeleteUserOperationClaimCommandValidator()
+    {
+        RuleFor(d => d.UserId)
+            .NotEmpty()
+            .NotNull();
+        RuleFor(d => d.OperationClaimId)
+            .NotEmpty()
+            .NotNull();
+    }
+}
