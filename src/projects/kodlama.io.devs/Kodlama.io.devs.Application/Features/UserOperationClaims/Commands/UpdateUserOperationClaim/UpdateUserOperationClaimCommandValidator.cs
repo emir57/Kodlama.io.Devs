e4b3c03972252e5a4f9866eb4b7a2e274c487b9b
emir57 <emir.gurbuz06@hotmail.com>
2022-10-09 00:27:35 +0300

    UpdateUserOperationClaimCommandValidator

---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Features/UserOperationClaims/Commands/UpdateUserOperationClaim/UpdateUserOperationClaimCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Kodlama.io.devs.Application.Features.UserOperationClaims.Commands.UpdateUserOperationClaim;
+
+public sealed class UpdateUserOperationClaimCommandValidator : AbstractValidator<UpdateUserOperationClaimCommand>
+{
+    public UpdateUserOperationClaimCommandValidator()
+    {
+        RuleFor(u => u.UserId)
+            .NotEmpty()
+            .NotNull();
+        RuleFor(u => u.OperationClaimId)
+            .NotEmpty()
+            .NotNull();
+    }
+}
