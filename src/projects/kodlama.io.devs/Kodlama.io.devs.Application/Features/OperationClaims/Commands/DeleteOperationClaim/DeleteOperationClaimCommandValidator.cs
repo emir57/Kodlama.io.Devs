ff9ba1452799b1a4cbd850410afbbf5c594586bd
emir57 <emir.gurbuz06@hotmail.com>
2022-10-08 23:58:34 +0300

    DeleteOperationClaimCommandValidator

---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Features/OperationClaims/Commands/DeleteOperationClaim/DeleteOperationClaimCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Kodlama.io.devs.Application.Features.OperationClaims.Commands.DeleteOperationClaim;
+
+public sealed class DeleteOperationClaimCommandValidator : AbstractValidator<DeleteOperationClaimCommand>
+{
+    public DeleteOperationClaimCommandValidator()
+    {
+        RuleFor(d => d.Id)
+            .NotEmpty()
+            .NotNull();
+    }
+}
