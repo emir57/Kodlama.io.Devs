2661478ff5fa5d8ab52bbf2ec412f14380f3034b
emir57 <emir.gurbuz06@hotmail.com>
2022-10-08 23:45:33 +0300

    CreateOperationClaimCommandValidator

---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Features/OperationClaims/Commands/CreateOperationClaim/CreateOperationClaimCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Kodlama.io.devs.Application.Features.OperationClaims.Commands.CreateOperationClaim;
+
+public sealed class CreateOperationClaimCommandValidator : AbstractValidator<CreateOperationClaimCommand>
+{
+    public CreateOperationClaimCommandValidator()
+    {
+        RuleFor(c => c.Name)
+            .NotNull()
+            .NotEmpty();
+
+        RuleFor(c => c.Name)
+            .MaximumLength(50);
+    }
+}
