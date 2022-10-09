f729ad17b6df1f27f064c9dffa2460846e7001f8
emir57 <emir.gurbuz06@hotmail.com>
2022-10-09 13:44:31 +0300

    GetByIdProgrammingLanguageQueryValidator

---
+++ src/projects/kodlama.io.devs/Kodlama.io.devs.Application/Features/ProgrammingLanguages/Queries/GetByIdProgrammingLanguage/GetByIdProgrammingLanguageQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Kodlama.io.devs.Application.Features.ProgrammingLanguages.Queries.GetByIdProgrammingLanguage;
+
+public sealed class GetByIdProgrammingLanguageQueryValidator : AbstractValidator<GetByIdProgrammingLanguageQuery>
+{
+    public GetByIdProgrammingLanguageQueryValidator()
+    {
+        RuleFor(g => g.Id)
+            .NotEmpty()
+            .NotNull();
+    }
+}
